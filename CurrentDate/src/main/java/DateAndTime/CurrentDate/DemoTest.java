a43217c07626aa0cb142f784229bab0a8f74ec9b
RupaGowd <RupaGowd@DESKTOP-NLFF423>
2019-11-11 14:52:40 +0530

    Commited

---
+++ CurrentDate/src/main/java/DateAndTime/CurrentDate/DemoTest.java
@@ -0,0 +1,13 @@
+package DateAndTime.CurrentDate;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("Welcome to Maven World");
+	}
+
+}
